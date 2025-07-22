1799a6d424592bbc1471074f3988a31b74e17ec9
Miguel Linares <ifmlinares@gmail.com>
2025-07-22 15:16:42 -0400

    Update ALLOWED_HOSTS to initialize with an empty string

--- settings/settings.py
+++ settings/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-07oda$9j6dc!g-0#quddc+7e4_3zzxv4e(d2v^naz&!lhcb8e#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
